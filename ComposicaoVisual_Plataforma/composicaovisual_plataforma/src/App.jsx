28ee86d9b174f9c72965192697851c65c33eee3a
Pedro Garica <1190956@isep.ipp.pt>
2023-10-07 16:58:41 +0100

    Further development of the main page

--- ComposicaoVisual_Plataforma/composicaovisual_plataforma/src/App.jsx
+++ ComposicaoVisual_Plataforma/composicaovisual_plataforma/src/App.jsx
@@ -1,40 +1,52 @@
-import { useState } from 'react'
-import banner1 from './assets/banner-1-image.jpg'
+import banner1 from './assets/banner-1-image_.jpg'
 import './App.css'
+import { useState } from 'react'
+
+const Pages = { HOME: "HOME", INFO: "INFO", TICKETS: "TICKETS" }
 
 function App() {
-    const [count, setCount] = useState(0)
+    const [currentPage, setCurrentPage] = useState(Pages.HOME);
 
+    const displayPage = () => {
+        switch (currentPage) {
+            case Pages.INFO:
+                return displayHomePage(); //displayInfoPage()
+            case Pages.TICKETS:
+                return displayHomePage(); //displayTicketsPage()
+            default:
+                return displayHomePage();
+        }
+    }
+
+    const displayHomePage = () => {
+        return (
+            <>
+                <div className="banner">
+                    <img src={banner1} alt="banner" />
+                    <p>Hell on Water</p>
+                </div>
+                <div className="card">
+                    <p>PORTO - PORTUGAL</p>
+                    <p>13 &gt; 15 AUGUST</p>
+                </div>
+            </>
+        )
+    }
     return (
         <>
             <div className="navbar">
-                <h1>HW</h1>
+                <h1 className="text-button" onClick={() => setCurrentPage(Pages.INFO)}>HW</h1>
                 <div className="navbar-items">
-                    <a>INFO</a>
-                    <a>TICKETS</a>
+                    <p className="text-button" onClick={() => setCurrentPage(Pages.INFO)}>INFO</p>
+                    <p className="text-button" onClick={() => setCurrentPage(Pages.TICKETS)}>TICKETS</p>
                     <div className="navbar-language">
-                        <a>PT</a>
-                        <a>|</a>
-                        <a>EN</a>
+                        <p className="text-button">PT</p>
+                        <p>|</p>
+                        <p className="text-button">EN</p>
                     </div>
                 </div>
             </div>
-            <div className="banner">
-                <img src={banner1} className="banner-image" alt="banner" />
-                <a>Hell on Water</a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
+            {displayPage()}
         </>
     )
 }
