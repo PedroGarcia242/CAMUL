1b8c46b0b9fa1322c078af4751dfb4ce737f443d
Pedro Garcia <1190956@isep.ipp.pt>
2023-11-28 19:26:45 +0000

    Commented some lines

--- Platform_MOOC/platform_mooc/src/App.jsx
+++ Platform_MOOC/platform_mooc/src/App.jsx
@@ -1,27 +1,34 @@
-import banner1 from './assets/banner-1-image.jpg'
-import banner2 from './assets/hw_poster.png'
-import banner3 from './assets/infoSection_background.jpg'
 import './App.css'
 import { useState } from 'react'
 
+//definition of possible pages
 const Pages = { HOME: "HOME", INFO: "INFO", TICKETS: "TICKETS" }
 
+//THIS IS CURRENTLY A ONE PAGE APP, WE CAN CHANGE IT LATER
 function App() {
+    //create a variable, assign a value
     const [currentPage, setCurrentPage] = useState(Pages.HOME);
 
+    //FUNCTIONS
+    
+    //displays the content of the currently selected page
     const displayPage = () => {
+        //check currentPage
         switch (currentPage) {
-            case Pages.INFO:
-                return displayInfoPage();
-            case Pages.TICKETS:
-                return displayTicketsPage();
-            default:
-                return displayHomePage();
+            case Pages.INFO: //if currentPage == Pages.INFO 
+                return displayInfoPage(); //display InfoPage content
+            case Pages.TICKETS: //if currentPage == Pages.Tickets 
+                return displayTicketsPage(); //display TicketsPage content
+            default: //else 
+                return displayHomePage(); //display HomePage content
         }
     }
 
+    //home Page content
     const displayHomePage = () => {
+        //returns ONE html element
         return (
+            //create one element and put the desired html inside
             <>
                 <div className="content">
                     <h3>
@@ -32,6 +39,7 @@ function App() {
         )
     }
 
+    //info Page content
     const displayInfoPage = () => {
         return (
             <>
@@ -42,6 +50,7 @@ function App() {
         )
     }
 
+    //tickets Page content
     const displayTicketsPage = () => {
         return (
             <>
@@ -52,32 +61,43 @@ function App() {
         )
     }
 
+    //RETURN
+    //the base content of the page
     return (
         <>
             <div className="navbar">
                 <h1 className="text-button" onClick={() => {
                     setCurrentPage(Pages.HOME)
                     window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
-                }
-                }>HOME</h1>
+                }}>
+                    HOME
+                </h1>
+
                 <div className="navbar-items">
                     <p className="text-button" onClick={() => {
                         setCurrentPage(Pages.INFO)
                         window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
-                    }}>PROFILE</p>
+                    }}>
+                        PROFILE
+                    </p>
+
                     <p className="text-button" onClick={() => {
                         setCurrentPage(Pages.TICKETS)
                         window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
-                    }}>MENU</p>
+                    }}>
+                        MENU
+                    </p>
                 </div>
             </div>
+
             {displayPage()}
+
             <div className="footer">
                 <div className="footer-items">
-                <p className="text-button">Contact</p>
-                <p>|</p>
-                <p className="text-button">General Information</p>
-                <p>|</p>
+                    <p className="text-button">Contact</p>
+                    <p>|</p>
+                    <p className="text-button">General Information</p>
+                    <p>|</p>
                     <p className="text-button">Media</p>
                 </div>
             </div>
