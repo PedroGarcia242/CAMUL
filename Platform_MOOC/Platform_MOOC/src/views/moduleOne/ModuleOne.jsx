58a3b520a2521ede9b81b5b4b138869f23ef43c7
Pedro Garcia <1190956@isep.ipp.pt>
2023-12-07 12:40:29 +0000

    nao está responsivo, depois corrijo

--- Platform_MOOC/Platform_MOOC/src/views/moduleOne/ModuleOne.jsx
+++ Platform_MOOC/Platform_MOOC/src/views/moduleOne/ModuleOne.jsx
@@ -25,11 +25,33 @@ const ModuleOne = () => {
             <div className="content-module-one">
                 <img className="background" src={module_one_background} alt="" />
                 <div className="video-container">
+                    <div className="video-background" />
                     <iframe
                         src="https://www.youtube.com/embed/5V5QDihsihY"
                         frameBorder="0"
                         allowFullScreen
                     ></iframe>
+                </div>
+                <div className="video-description">
+                    <div className="video-description-background">
+                    <h1>THIS LESSON</h1>
+                    </div>
+                    <div className="video-description-background-hole" >
+                        <h1> &gt; TRANSCRIPTS</h1>
+                    </div>
+                </div>
+
+                <div className="chapter-description">
+                    <div className="chapter-description-background" >
+                        <h1>CHAPTER 1: Introduction to Communication and Marketing</h1>
+                    </div>
+                    <div className="chapter-description-background-hole" >
+                        <h1> &gt; Lesson 1: </h1>
+                        <h1> &gt; Lesson 2: </h1>
+                        <h1> &gt; Lesson 3: </h1>
+                        <h1> &gt; Lesson 4: </h1>
+                        <h1> &gt; Lesson 5: </h1>
+                    </div>
                 </div>
             </div>
             {displayFooter()}
