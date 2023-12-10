2fb0cea7c6d308b81c028a08e6c1aaa3482fcb74
Pedro Garica <1190956@isep.ipp.pt>
2023-12-10 19:13:54 +0000

    home page updates

--- Platform_MOOC/Platform_MOOC/src/views/Home.jsx
+++ Platform_MOOC/Platform_MOOC/src/views/Home.jsx
@@ -1,36 +1,135 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useNavigate } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
-import main_page_background from '../assets/module_two_bg.png';
-import standalone_image from '../assets/standalone_image.png';
+import homeBackground from '../assets/home.png';
+import transcriptImage from '../assets/transcriptImage.png';
 
 const Home = () => {
+
     const navigate = useNavigate();
 
-    const handleHomeClick = () => {
-        navigate('/');
+    const handleModuleOneClick = () => {
+        navigate('/ModuleOne/1');
+        window.scrollTo({ top: 0, behavior: 'smooth' });
     };
 
-    //footer
+    const handleModuleTwoL1Click = () => {
+        navigate('/ModuleTwoL1');
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+
+    const handleModuleThreeClick = () => {
+        navigate('/ModuleThree/1');
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+
+
     const displayFooter = () => {
         return (
             <>
-                <div className="footer-main-page">
+                <div className="footer-home-page">
                 </div>
             </>
         )
     }
 
+    const moduleOneBlock = () => {
+        return (
+            <>
+                <div className="block" onClick={handleModuleOneClick} >
+                    <div className="block-content">
+                        <h3 className="block-title">
+                            Module 1
+                        </h3>
+                        <h5 className="block-text">
+                            Learn the basics of Integrated Marketing Communication
+                        </h5>
+                    </div>
+                </div>
+            </>
+        )
+    }
+
+    const moduleTwoBlock = () => {
+        return (
+            <>
+                <div className="block" onClick={handleModuleTwoL1Click} >
+                    <div className="block-content">
+                        <h3 className="block-title">
+                            Module 2
+                        </h3>
+                        <h5 className="block-text">
+                            Learn about the ethics and regulations present in the Marketing field
+                        </h5>
+                    </div>
+                </div>
+            </>
+        )
+    }
+
+    const moduleThreeBlock = () => {
+        return (
+            <>
+                <div className="block" onClick={handleModuleThreeClick} >
+                    <div className="block-content">
+                        <h3 className="block-title">
+                            Module 3
+                        </h3>
+                        <h5 className="block-text">
+                            Learn how to create, manage and monitorize a Marketing Plan
+                        </h5>
+                    </div>
+                </div>
+            </>
+        )
+    }
+
+
+
     return (
         <>
-            <div className="content-mainpage">
+            <div className="content-home-page">
+                <img className="background" src={homeBackground} alt="" />
 
-                <img className="background" src={main_page_background} alt="" />
+                <div className="home-header">
+                    <h2>Learn With Us</h2>
+                    <div className="home-header-content">
+                        <h5>Looking for guidance on where to start your marketing journey? You came to the right place.</h5>
+                        <h5>Explore our content, learn for free.</h5>
+                    </div>
+                </div>
 
-                <img className="promo_bit" src={standalone_image} alt="" />
+                <Container className="mb-4">
+                    <Row>
+                        <Col xl={8}>
+                            <div className="video-container-home-page">
+                                <div className="video-background-home-page">
+                                    <iframe
+                                        src="https://www.youtube.com/embed/Ufs0Y6HkPD4"
+                                        frameBorder="0"
+                                        allowFullScreen
+                                    ></iframe>
+                                </div>
+                            </div>
+                        </Col>
+                        <Col xl={4}>
+                            <div className="mooc-details">
+                                {moduleOneBlock()}
+                                {moduleTwoBlock()}
+                                {moduleThreeBlock()}
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
 
-                <a href="#" className="contents-button">SEE CONTENTS</a> 
-               
+                <div className="module-details pt-4">
+                    <h1>Module Details</h1>
+                    <div className="module-details-content">
+                    </div>
+                </div>
             </div>
             {displayFooter()}
         </>
--- Platform_MOOC/Platform_MOOC/src/views/Info.jsx
+++ Platform_MOOC/Platform_MOOC/src/views/Info.jsx
@@ -1,125 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Info.css';
-import { useNavigate } from 'react-router-dom';
-
-import module_one_background from '../assets/module_one_bg.png';
-import transcriptImage from '../assets/transcriptImage.png';
 
 const Info = () => {
-
-    const navigate = useNavigate();
-
-    const handleModuleOneClick = () => {
-        navigate('/ModuleOne/1');
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    };
-
-    const handleModuleTwoL1Click = () => {
-        navigate('/ModuleTwoL1');
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    };
-
-    const handleModuleThreeClick = () => {
-        navigate('/ModuleThree/1');
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    };
-
-
-    const displayFooter = () => {
-        return (
-            <>
-                <div className="footer-info-page">
-                </div>
-            </>
-        )
-    }
-
-    const moduleOneBlock = () => {
-        return (
-            <>
-                <div className="card" onClick={handleModuleOneClick} >
-                    <img className="card-img" src={transcriptImage} alt="transcript" />
-                    <div className="card-content">
-                        <h1 className="card-title">
-                            Module 1
-                        </h1>
-                        <h4 className="card-text">
-                            Learn the basics of Integrated Marketing Communication
-                        </h4>
-                    </div>
-                </div>
-            </>
-        )
-    }
-
-    const moduleTwoBlock = () => {
-        return (
-            <>
-                <div className="card" onClick={handleModuleTwoL1Click} >
-                    <img className="card-img" src={transcriptImage} alt="transcript" />
-                    <div className="card-content">
-                        <h1 className="card-title">
-                            Module 2
-                        </h1>
-                        <h4 className="card-text">
-                            Learn about the ethics and regulations present in the Marketing field
-                        </h4>
-                    </div>
-                </div>
-            </>
-        )
-    }
-
-    const moduleThreeBlock = () => {
-        return (
-            <>
-                <div className="card" onClick={handleModuleThreeClick} >
-                    <img className="card-img" src={transcriptImage} alt="transcript" />
-                    <div className="card-content">
-                        <h1 className="card-title">
-                            Module 3
-                        </h1>
-                        <h4 className="card-text">
-                            Learn how to create, manage and monitorize a Marketing Plan
-                        </h4>
-                    </div>
-                </div>
-            </>
-        )
-    }
-
+    
 
 
     return (
         <>
-            <div className="content-info-page">
-                <img className="background" src={module_one_background} alt="" />
-                <div className="video-container">
-                    <div className="video-background-info-page" />
-                    <iframe
-                        src="https://www.youtube.com/embed/Ufs0Y6HkPD4"
-                        frameBorder="0"
-                        allowFullScreen
-                    ></iframe>
-                </div>
-
-                <div className="video-description-info-page">
-                    <h1>Everything about Intregrated Marketing Communication in three Modules</h1>
-                    <div className="video-description-content">
-                        {moduleOneBlock()}
-                        {moduleTwoBlock()}
-                        {moduleThreeBlock()}
-                    </div>
-                </div>
-
-                <div className="module-details">
-                    <h1>Module Details</h1>
-                    <div className="module-details-content">
-                    </div>
-                </div>
-
-            </div>
-            {displayFooter()}
         </>
     );
 };
