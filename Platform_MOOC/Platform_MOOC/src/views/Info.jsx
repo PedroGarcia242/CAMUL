f27b52fc7b65a1c31a316b62de09a685595b3f54
Joáo Oliveira <1191457@isep.ipp.pt>
2023-12-10 21:59:48 +0000

    info page changes

--- Platform_MOOC/Platform_MOOC/src/views/Info.jsx
+++ Platform_MOOC/Platform_MOOC/src/views/Info.jsx
@@ -1,5 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import searchIcon from '../assets/SEARCHBAR2.png';
 
 import infoBackground from '../assets/INFO_BG.png';
 
@@ -26,29 +25,50 @@ const Info = () => {
                     </div>
                 </div>
 
-                <div className="search-container">
-                    <div className="search-input-wrapper">
-                    <input
-                        type="text"
-                        placeholder="Search..."
-                        className="search-input"
-                        readOnly // Prevents user input in the field
-                            style={{
-                            fontSize: '18px', // Increase font size of the input text
-                            backgroundImage: `url(${searchIcon})`,
-                            backgroundSize: 'contain', // Fit the image within the input
-                            backgroundRepeat: 'no-repeat', // Ensure the image doesn't repeat
-                            backgroundPosition: '10px 50%', // Adjust background position
-                            paddingLeft: '150px', // Set padding to accommodate the width of the image
-                            height: '500px', // Adjust height as needed
-                            width: '500px', // Adjust width as needed
-                        }}
-                    />
-                    <button className="search-button" disabled>
-                            {/* This button can be disabled to prevent interaction */}
-                        Search
+                <div className="accordion" id="accordionExample">
+                <div className="container-fluid">
+                    <form className="d-flex justify-content-center" role="search">
+                        <input className="form-control me-2 text-input-info-page" type="search" placeholder="Search" aria-label="Search"/>
+                        <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            Accordion Item #1
                         </button>
+                    </h2>
+                    <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                        <div className="accordion-body">
+                            <strong>This is the first items accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. Its also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                        </div>
                     </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            Accordion Item #2
+                        </button>
+                    </h2>
+                    <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div className="accordion-body">
+                            <strong>This is the second items accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. Its also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            Accordion Item #3
+                        </button>
+                    </h2>
+                    <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div className="accordion-body">
+                            <strong>This is the third items accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. Its also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                        </div>
+                    </div>
+                </div>
                 </div>
 
                 <div className="video-container">
